adc40c9cffae8436faad4f80adec61172dc163fd
Vineel Mallepalli <vineelmallepalli@Vineels-MBP.lan>
2024-03-26 10:22:13 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app-ec2" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "app-ec2"
+    }
+}
